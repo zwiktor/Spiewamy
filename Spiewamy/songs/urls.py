25b15775795d76a3605e83e9b39dafbb8cd2da92
Wiktor <zwiktor2023032@gmail.com>
2020-09-26 15:59:39 +0200

    Podstawowe ładowanie strony do spiewania skończone. Stworzenie aplikacji do logowania i rejestracji

--- Spiewamy/songs/urls.py
+++ Spiewamy/songs/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import home_song_view, home_view
+from .views import singroom_view, home_view
 
 urlpatterns = [
     path('', home_view),
-    path('<str:username>/', home_song_view),
+    path('<str:username>/', singroom_view),
 ]
--- Spiewamy/songs/views.py
+++ Spiewamy/songs/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render, redirect
-from django.http import request, HttpResponse
-from .models import Song, User
+from django.http import request, HttpResponse, Http404
+from .models import Song, User, SingRoom
 
 def home_view(request, *args, **kwargs):
     if request.method == 'GET':
+        return render(request, 'home.html')# pierwsze wejscie na strone
 
-        return render(request, 'home.html')
     elif request.method == 'POST':
-        username = request.POST['username']
-        return redirect(f'/{username}')
+        username = request.POST['username']#przypisanie danych z httprequest
+        is_user = User.objects.get(username=username)
+        if is_user:
+            return redirect(f'/{username}')
+        else:
+            raise Http404('Uzytkownik nie istnieje')
 
+def singroom_view(request, username,  *args, **kwargs):
+    user_id = User.objects.get(username=username).id
 
-def home_song_view(request, *args, **kwargs):
-    # tutaj logika chanelsow
-    return HttpResponse('this is user webpage')
+    songroom = SingRoom.objects.get(user_id=user_id)
+    if not songroom.song:
+        raise Http404('Uzytkownik nie spiewa')
+    song = Song.objects.get(id=songroom.song_id)
+    return render(request, 'singroom.html', context={'song': song})
 
 
